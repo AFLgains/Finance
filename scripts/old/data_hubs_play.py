67e15a62721e6a94c2c9abc7b1f0ce64693e1f47
Ric <porteous.ric@bcg.com>
2020-06-13 16:59:42 +1000

    Clean up strategies

--- scripts/old/data_hubs_play.py
+++ scripts/old/data_hubs_play.py
@@ -1,12 +1,9 @@
-import pandas as pd
 import os
 
+import pandas as pd
 # Import the main functionality from the SimFin Python API.
 import simfin as sf
-
-# Import names used for easy access to SimFin's data-columns.
 from simfin.names import *
-
 
 sf.set_api_key("AH4RWcm8001Mmz48dGzbZ13LGp1O5Yqw")
 
@@ -14,23 +11,25 @@ sf.set_api_key("AH4RWcm8001Mmz48dGzbZ13LGp1O5Yqw")
 # The dir will be created if it does not already exist.
 sf.set_data_dir(os.path.join("/", "raw_data"))
 
-market = 'us'
-tickers = ['AAPL', 'AMZN', 'MSFT','DIS','dasdasd']
+market = "us"
+tickers = ["AAPL", "AMZN", "MSFT", "DIS", "dasdasd"]
 offset = pd.DateOffset(days=0)
 refresh_days = 30
 
 # Refresh the dataset with shareprices every 10 days.
 refresh_days_shareprices = 10
 
-hub = sf.StockHub(market=market, tickers=tickers, offset=offset,
-                  refresh_days=refresh_days,
-                  refresh_days_shareprices=refresh_days_shareprices)
+hub = sf.StockHub(
+    market=market,
+    tickers=tickers,
+    offset=offset,
+    refresh_days=refresh_days,
+    refresh_days_shareprices=refresh_days_shareprices,
+)
 
 
-df_income_annual = hub.load_income(variant='ttm')
-df_fin_signals_daily = hub.fin_signals(variant='daily')
-df_growth_signals_daily = hub.growth_signals(variant='daily')
-df_val_signals_latest = hub.val_signals(variant='daily')
-df_income_annual = hub.load_income(variant='ttm')
-
-
+df_income_annual = hub.load_income(variant="ttm")
+df_fin_signals_daily = hub.fin_signals(variant="daily")
+df_growth_signals_daily = hub.growth_signals(variant="daily")
+df_val_signals_latest = hub.val_signals(variant="daily")
+df_income_annual = hub.load_income(variant="ttm")
